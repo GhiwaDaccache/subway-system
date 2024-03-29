ae2557e0523e4e16a22fd7082195fdad3489f360
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-03-29 09:51:31 +0200

    first commit

---
+++ subway-react/src/App.jsx
@@ -0,0 +1,23 @@
+import "./styles/utilities.css";
+import "./styles/colors.css";
+
+import { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+
+const App = () => {
+  useEffect(() => {}, []);
+
+  return(
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App;
