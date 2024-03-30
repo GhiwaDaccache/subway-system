0f2b8a354afff2a78c96a348ffc05980d6f7f77d
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-03-30 10:01:19 +0200

    Created and Styled Signup Page

--- frontend/src/pages/authentication/index.jsx
+++ frontend/src/pages/authentication/index.jsx
@@ -4,7 +4,49 @@ import './auth.css';
 function Authentication() {
   return (
     <div className="auth_container">
-      <h1>Authentication</h1>
+      <Background />
+      <Form />
+      <div className="overlay"></div>
+    </div>
+  );
+}
+
+function Background() {
+  return <div className="auth_bg_container"></div>;
+}
+
+function Form() {
+  return (
+    <div className="auth_submit_container">
+      <div className="switch_interfaces_container flex align-center justify-center">
+        <span className="auth_active">Sign up</span>
+        <span className="auth_hidden">Login</span>
+      </div>
+
+      <form className="auth_form_container">
+        <fieldset className="flex column">
+          <label htmlfor="user-name">User Name</label>
+          <input id="user-name" type="text" required />
+        </fieldset>
+
+        <fieldset className="flex column">
+          <label htmlfor="user-email">User Email</label>
+          <input id="user-email" type="email" required />
+        </fieldset>
+
+        <fieldset className="flex column">
+          <label htmlfor="password">Password</label>
+          <input id="password" type="text" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$" required />
+        </fieldset>
+
+        <fieldset className="check_fieldset flex row">
+          <input type="checkbox" />
+          <label>Remember Me</label>
+        </fieldset>
+
+        <button type="submit"></button>
+      </form>
+      <p className="guest_text">Enter as a Guest</p>
     </div>
   );
 }
