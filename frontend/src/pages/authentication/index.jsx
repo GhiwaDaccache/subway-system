3352e6560c8142f0a709f138bedc341c16f6d007
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-03-30 12:14:28 +0200

    Fixed-Responsiveness-Bugs

--- frontend/src/pages/authentication/index.jsx
+++ frontend/src/pages/authentication/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './auth.css';
 
 function Authentication() {
@@ -16,38 +16,79 @@ function Background() {
 }
 
 function Form() {
+  const [toggleLoginRegisterInterfaces, setToggleLoginRegisterInterfaces] = useState(true);
+
   return (
     <div className="auth_submit_container">
       <div className="switch_interfaces_container flex align-center justify-center">
-        <span className="auth_active">Sign up</span>
-        <span className="auth_hidden">Login</span>
+        <span
+          onClick={() => {
+            setToggleLoginRegisterInterfaces(true);
+          }}
+          className={toggleLoginRegisterInterfaces ? 'auth_active' : 'auth_hidden'}
+        >
+          Sign up
+        </span>
+        <span
+          onClick={() => {
+            setToggleLoginRegisterInterfaces(false);
+          }}
+          className={!toggleLoginRegisterInterfaces ? 'auth_active' : 'auth_hidden'}
+        >
+          Login
+        </span>
       </div>
 
-      <form className="auth_form_container">
-        <fieldset className="flex column">
-          <label htmlfor="user-name">User Name</label>
-          <input id="user-name" type="text" required />
-        </fieldset>
+      {toggleLoginRegisterInterfaces ? (
+        <form className="auth_form_container">
+          <fieldset className="flex column">
+            <label htmlFor="user-name">User Name</label>
+            <input id="user-name" type="text" required />
+          </fieldset>
 
-        <fieldset className="flex column">
-          <label htmlfor="user-email">User Email</label>
-          <input id="user-email" type="email" required />
-        </fieldset>
+          <fieldset className="flex column">
+            <label htmlFor="user-email">User Email</label>
+            <input id="user-email" type="email" required />
+          </fieldset>
 
-        <fieldset className="flex column">
-          <label htmlfor="password">Password</label>
-          <input id="password" type="text" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$" required />
-        </fieldset>
+          <fieldset className="flex column">
+            <label htmlFor="password">Password</label>
+            <input id="password" type="text" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$" required />
+          </fieldset>
 
-        <fieldset className="check_fieldset flex row">
-          <input type="checkbox" />
-          <label>Remember Me</label>
-        </fieldset>
+          <fieldset className="check_fieldset flex row">
+            <input type="checkbox" />
+            <label>Remember Me</label>
+          </fieldset>
 
-        <button className="submit_btn" type="submit">
-          Submit
-        </button>
-      </form>
+          <button className="submit_btn" type="submit">
+            Submit
+          </button>
+        </form>
+      ) : (
+        <form className="auth_form_container">
+          <fieldset className="flex column">
+            <label htmlFor="user-email">User Email</label>
+            <input id="user-email" type="email" required />
+          </fieldset>
+
+          <fieldset className="flex column">
+            <label htmlFor="password">Password</label>
+            <input id="password" type="text" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$" required />
+            <span className="forgot_password_text">forgot password?</span>
+          </fieldset>
+
+          <fieldset className="check_fieldset flex row">
+            <input type="checkbox" />
+            <label>Remember Me</label>
+          </fieldset>
+
+          <button className="submit_btn" type="submit">
+            Submit
+          </button>
+        </form>
+      )}
+
       <p className="guest_text">Enter as a Guest</p>
     </div>
   );
