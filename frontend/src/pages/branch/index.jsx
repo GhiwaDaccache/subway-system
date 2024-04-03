41278a6f14a28ecd027b5ec3caf3f1b1ed812c34
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-04-03 13:15:20 +0300

    fixed-reviews-ui

--- frontend/src/pages/branch/index.jsx
+++ frontend/src/pages/branch/index.jsx
@@ -1,35 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import StationInfo from './components/StationInfo';
-import RideCard from './components/RideCard';
-import RideCardForm from './components/RideCardForm';
 
-import AddIcon from '@mui/icons-material/Add';
-
+import { Outlet } from 'react-router';
 import './branch.css';
 
 function Branch() {
   return (
     <div className="page column">
-
       <Navbar /> 
-      <section className='flex branch-page'>
-        <StationInfo/>
-        <section className='rides-container flex column align-center gap'>
-          <div className='flex align-center full-w'>
-            <AddIcon className='icon'/>
-            <RideCardForm />
-          </div>
-          <div className="flex align-center full-w gap">
-            <RideCard />
-            <RideCard />
-            <RideCard />
-            <RideCard />
-            <RideCard />
-            <RideCard />
-          </div>
-        </section>
-      </section>
+      <Outlet />
     </div>
   );
 }
