e2b4b0db6e7fa64d8b5662964ae71297835b4d0d
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-04-04 22:41:49 +0300

    edited-branch-card

--- frontend/src/pages/admin/index.jsx
+++ frontend/src/pages/admin/index.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import './components/sidebar.css';
 import './index.css';
 import Sidebar from './components/sidebar';
-import Dashboard from './pages/dashboard';
-import CoinRequest from './pages/coin-requests';
-import InviteManagers from './pages/add-managers';
-import EditBranches from './pages/edit-branches';
 import { Outlet } from 'react-router';
 
 function Admin() {
--- frontend/src/pages/admin/pages/edit-branches/components/branch-card.jsx
+++ frontend/src/pages/admin/pages/edit-branches/components/branch-card.jsx
@@ -1,29 +1,37 @@
-function ViewBranches({station}) {
-  const {name,location,opening_time,closing_time}=station;
-    return (
-      <div className="branch-cards flex   justify-around">
-        <div className=" flex column  justify-around">
-          <div>Branch Name:</div>
-          <div>Location:</div>
+function ViewBranches({ station }) {
+  const { id, name, location, opening_time, closing_time, status } = station;
+
+  const isActive = status == 0 ? "Activate" : "Deactivate";
+
+  return (
+    <div className="branch-cards flex   justify-around">
+      <div className=" flex column  justify-around">
+        <div>Branch Name:</div>
+        <div>Location:</div>
+      </div>
+      <div className=" flex column  justify-around">
+        <div>{name}</div>
+        <div>{location}</div>
+      </div>
+      <div className=" flex column justify-around">
+        <div>Opening hour:</div>
+        <div>Closing hour:</div>
+      </div>
+      <div className=" flex column  justify-around">
+        <div>{opening_time}</div>
+        <div>{closing_time}</div>
+      </div>
+      <div className=" flex column  justify-around">
+        <div>
+          {" "}
+          <button className="branch-buttons">{isActive}</button>
         </div>
-        <div className=" flex column  justify-around">
-          <div>{name}</div>
-          <div>{location}</div>
-        </div>
-        <div className=" flex column justify-around">
-          <div>Opening hour:</div>
-          <div>Closing hour:</div>
-        </div>
-        <div className=" flex column  justify-around">
-          <div>{opening_time}</div>
-          <div>{closing_time}</div>
-        </div>
-        <div className=" flex column  justify-around">
-       <div> <button className="branch-buttons">Disable</button></div>
-       <div> <button className="branch-buttons">Remove</button></div>
+        <div>
+          {" "}
+          <button className="branch-buttons">Remove</button>
         </div>
       </div>
-    );
-  }
-  export default ViewBranches;
-  
+    </div>
+  );
+}
+export default ViewBranches;
