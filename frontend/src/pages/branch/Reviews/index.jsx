f64b4ede2b31074e6b9f2218e8ea0be3648e0ae6
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-04-05 03:52:26 +0300

    linked-review-with-token

--- frontend/src/pages/branch/Reviews/index.jsx
+++ frontend/src/pages/branch/Reviews/index.jsx
@@ -27,60 +27,67 @@ function Reviews() {
 
 
 
-  const handleStatusChange = (index, newStatus) => {
-    const updatedReviews = [...reviews];
-    updatedReviews[index].status = newStatus;
-    setReviews(updatedReviews);
-  };
+    const handleStatusChange = (index, newStatus) => {
+      const updatedReviews = [...reviews];
+        updatedReviews[index].status = newStatus;
+        setReviews(updatedReviews);
 
-  return (
-    <div className="page flex center">
-      <table className="reviews-table">
-        <thead>
-          <tr className="reviews-th white-text">
-            <th>Description</th>
-            <th>Date</th>
-            <th>Ride</th>
-            <th>Status</th>
-          </tr>
-        </thead>
+        axios.post('http://localhost:8000/api/approve_reviews', {
+        review_id: updatedReviews[index].id,
+        approved: newStatus === 'approved' ? 1 : 0
+      }).then(response => {
+        console.log(response.data.message);
+      }).catch(error => {
+        console.error('Error updating status:', error);
+      });
+       
 
-        <tbody>
-          {reviews.map((value, index) => {
-            return (
-              <tr className="reviews-tr" key={value.id}>
-                <td>{value.description}</td>
-                <td>{new Date(value.created_at).toLocaleDateString()}</td>
-                <td>{value.ride_id}</td>
-                <td>
-                  {value.status === null ? (
-                    <NullStatus index={value.id} onStatusChange={handleStatusChange} />
-                  ) : value.status === 1 ? (
-                    <CheckCircleIcon />
-                  ) : (
-                    <CancelIcon />
-                  )}
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+    };
 
-
-    </div>
-  );
-}
-
-
-function NullStatus({ index, onStatusChange }) {
-  return (
-    <div className="flex a-center justify-evenly">
-      <CheckCircleOutlineIcon value="approved" className="custom_status_icons" onClick={() => onStatusChange(index, 'approved')} />
-      <HighlightOffIcon value="declined" className="custom_status_icons" onClick={() => onStatusChange(index, 'declined')} />
-    </div>
-  );
-}
-
-export default Reviews;
-
+    return (
+      <div className="page flex center">
+        <table className="reviews-table">
+          <thead>
+            <tr className="reviews-th white-text">
+              <th>Description</th>
+              <th>Date</th>
+              <th>Ride</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+  
+          <tbody>
+            {reviews.map((value, index) => {
+              return (
+                <tr className="reviews-tr" key={index}>
+                  <td>{value.description}</td>
+                  <td>{new Date(value.created_at).toLocaleDateString()}</td>
+                  <td>{value.ride_id}</td>
+                  <td>
+                    {value.status === null ? (
+                      <NullStatus index={index} onStatusChange={handleStatusChange} />
+                    ) : value.status === 'approved' ? (
+                      <CheckCircleIcon />
+                    ) : (
+                      <CancelIcon />
+                    )}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+  
+  function NullStatus({ index, onStatusChange }) {
+    return (
+      <div className="flex a-center justify-evenly">
+        <CheckCircleOutlineIcon value="approved" className="custom_status_icons" onClick={() => onStatusChange(index, 'approved')} />
+        <HighlightOffIcon value="declined" className="custom_status_icons" onClick={() => onStatusChange(index, 'declined')} />
+      </div>
+    );
+  }
+  
+  export default Reviews;
